7bd211194a692f2fd79a5e6b904466b8babe7afb
sampu <sampu@LAPTOP-T75V57GF>
2021-02-24 16:17:20 +0530

    Hello file

---
+++ Project1/src/Package1/MyClass.java
@@ -0,0 +1,11 @@
+package Package1;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to DevOps Training");
+
+	}
+
+}
